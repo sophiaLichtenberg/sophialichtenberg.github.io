9f0e3f170ecbfa8ec029f5b5f4d55bb788af4989
sophiaLichtenberg <sophia-lichtenberg@gmx.de>
2024-09-02 16:06:36 +0200

    update form

--- docs/form.py
+++ docs/form.py
@@ -1,4 +1,10 @@
 import cgi
 form = cgi.FieldStorage()
-searchterm =  form.getvalue('searchbox')
-print(searchterm)
+
+name = form.getvalue('age')
+email = form.getvalue('sex')
+background = form.getvalue('background')
+
+print(name)
+print(email)
+print(background)
